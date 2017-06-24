820e132c8c8170f239831c87ee99f3479c308358
pedro_2 <pedro_2@pedro-perez>
2017-06-24 17:50:53 -0500

    Inicio Se alamacena t --se crea el metodo qeu alamacena t como numero de conultas

--- app/Bal/BalCube.php
+++ app/Bal/BalCube.php
@@ -26,20 +26,64 @@ class BalCube {
     public function EjecutarQuery($query) {
 
         $ltsquery = explode("\n", $query);
-        $queryData= new \App\Dal\QueryData( $this->request);
+        $queryData = new \App\Dal\QueryData($this->request);
         foreach ($ltsquery as &$exQuery) {
-         $queryData->GuardarQuery($exQuery);
+            $resultado = $this->EvaluarQuery($exQuery);
+            $queryData->GuardarQuery($exQuery . $resultado);
         }
 
         $data = new \App\Dal\CubeData($this->request);
-        if (!$this->Existe()) {
-            $data->GuadarMatriz(2);
-        }
-        return ["matriz"=>$data->GetMatriz($this->request),"historia"=>$queryData->GetHistoria()];
+
+        return ["matriz" => $data->GetMatriz($this->request), "historia" => $queryData->GetHistoria()];
     }
 
     function Existe() {
         return $this->request->session()->has('cubeSum');
+    }
+
+    function EvaluarQuery($query) {
+
+        $resultado = "";
+        $exQuerytrim = trim($query);
+        $tipo = $this->tipoQuery(strlen($exQuerytrim), $exQuerytrim);
+        $resultadoT = "Ejecutado " . $this->ExecuteQuery($tipo, $exQuerytrim);
+        $resultado = $resultado . "<br>" . $resultadoT;
+
+        return $resultado;
+    }
+
+    function tipoQuery($size, $query) {
+        if ($size == 1) {
+            return "T";
+        } else if ($size == 2) {
+            return "N-M";
+        } else {
+
+            if (strpos($query, "UPDATE")) {
+                return "UPDATE";
+            } else if (strpos($query, "QUERY")) {
+                return "QUERY";
+            } else {
+                return null;
+            }
+        }
+    }
+
+    function ExecuteQuery($tipo, $query) {
+        $data = new \App\Dal\CubeData($this->request);
+        if ($tipo == "UPDATE") {
+            return "UPDATE";
+        } else if ($tipo == "QUERY") {
+            return "QUERY";
+        } else if ($tipo == "T") {
+             $this->request->session()->put('numT', $query);
+            return "T se haran ".$this->request->session()->get('numT')." consultas.";
+        } else if ($tipo == "N-M") {
+             $data->GuadarMatriz($query);
+            return "N-M";
+        } else {
+            return null;
+        };
     }
 
 }
