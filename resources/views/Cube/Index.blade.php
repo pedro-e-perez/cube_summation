b9d1a343e1f432ba9311b0071c5be851d041c58e
pedro_2 <pedro_2@pedro-perez>
2017-06-24 14:50:30 -0500

    Creacion presentacion Se crea la interfaz de usuario donde el usuario escribira lso comandos y se mostrara el resultado al cubo -agregar textbox -Panel resultado -accion ejecutar -js layer page

--- resources/views/Cube/Index.blade.php
+++ resources/views/Cube/Index.blade.php
@@ -10,6 +10,44 @@
 
 @section('content')
 
-<h1>Cube Summation</h1>
+<h1>www.hackerrank.com</h1>
 
+<div class="panel panel-default">
+    <div class="panel-heading"><i class="fa fa-bar-chart" aria-hidden="true"></i> Resumen </div>
+    <div class="panel-body">
+        <div class="resumen">
+            <div class="contresumen"></div>
+            
+        </div>
+    </div>
+</div>
+<div class="panel panel-default">
+    <div class="panel-heading"><i class="fa fa-terminal" aria-hidden="true"></i> Queryes </div>
+    <div class="panel-body">
+        <div class="row">
+            <div class="col-lg-10">       
+                <textarea id="txtQuey" rows="4" cols="50" class="terminal">
+                </textarea>
+
+            </div> 
+            <div class="col-lg-2">
+                <button id="btnEjecutar" class="btn btn-success"><i class="fa fa-play" aria-hidden="true"></i> Ejecutar</button>
+            </div>
+
+        </div>   
+
+
+    </div>
+</div>
+
+
+@endsection
+@section('scriptsPage')
+
+<script src="../../../js/App/Cube.js" type="text/javascript"></script>
+
+<script>
+    $(document).ready(page.init());
+
+</script>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,11 +6,16 @@
 
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         <script src="https://ajax.aspnetcdn.com/ajax/jquery.validate/1.10.0/jquery.validate.min.js" type="text/javascript"></script>
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+        <link href="../../../css/app.css?v={{ Cache::get('js_version_number') }}" rel="stylesheet" type="text/css"/>
+        <link href="../../../css/hover.css?v={{ Cache::get('js_version_number') }}" rel="stylesheet" type="text/css"/>
     </head>
     <body>
         <style>
